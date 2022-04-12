7595dd1178f07c88b1cbc10c42c22656962455fa
Léderer András <andrew@file.glass>
2022-04-12 23:27:28 +0200

    chore: manual release ci

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ mod manif_loader;
 mod utils;
 mod rt;
 mod fs;
+use std::time::{Instant};
 
 use std::path::PathBuf;
 use colored::Colorize;
 use crate::manif_loader::load_manifest_in;
 
 fn main() {
+    let start = Instant::now();
     let args: Vec<String> = std::env::args().collect();
     println!("{}", "Finding your fxmanifest.lua file...".green().bold());
     let manif_re_path = args.get(1);
@@ -30,4 +32,6 @@ fn main() {
         dir.push("../wow/fxmanifest.lua");
         load_manifest_in(dir);
     }
+    let duration = start.elapsed();
+    println!("{}", format!("Done in {} ms", duration.as_millis()).green())
 }
